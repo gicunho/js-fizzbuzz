5b289736d5ad41262b0bc6e3b32b26b36870d5b1
gicunho <gicunho@hotmail.it>
2021-04-27 16:37:33 +0200

    Creato criteri di stampa Fizz e Buzz

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,9 +3,19 @@ var numero = [];
 
 for (i = 0; i < 100; i++) {
     var number = i + 1;
-    numero.push(number);
+    //Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+    if ((Number(number) % 3 == 0) && (Number(number) % 5 == 0)){
+        numero.push('FizzBuzz');
+    //ma per i multipli di 3 stampi “Fizz” al posto del numero e
+    } else if (Number(number) % 3 == 0){
+        numero.push('Fizz');
+    //per i multipli di 5 stampi Buzz.
+    } else if (Number(number) % 5 == 0){
+        numero.push('Buzz')
+    } else {
+        numero.push(number)
+    }
 }
+
 console.log(numero);    
-//ma per i multipli di 3 stampi “Fizz” al posto del numero e 
-//per i multipli di 5 stampi Buzz.
-//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+document.getElementById('numbers').innerHTML = numero + ' <br>';
