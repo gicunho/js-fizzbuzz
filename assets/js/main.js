ee1eb68a2d4ec0f83d1cd2896347516f76a9a445
gicunho <gicunho@hotmail.it>
2021-04-27 15:51:59 +0200

    Creato programma stampa numeri da 0 a 100

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+//Scrivi un programma che stampi i numeri da 1 a 100,
+var numero = [];
+
+for (i = 0; i < 100; i++) {
+    var number = i + 1;
+    numero.push(number);
+}
+console.log(numero);    
+//ma per i multipli di 3 stampi “Fizz” al posto del numero e 
+//per i multipli di 5 stampi Buzz.
+//Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
